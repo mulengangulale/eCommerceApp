7cead88b0ea2f0907bbd102db116c525d3a82195
unknown <ngulalemulenga@gmail.com>
2024-11-12 16:31:04 -0500

    Created dummy users for the app

---
+++ backend/users.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+import e from "express";
+
+const users = [
+    {
+        name: "Admin User",
+        email: "admin@email.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "John Doe",
+        email: "john@email.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: false,
+    },
+    {
+        name: "Jane Doe",
+        email: "jane@email.com",
+        isAdmin: false,
+    }
+];
+
+export default users;
