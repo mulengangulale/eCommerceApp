ae3b37bc2e73e66e886b9a75444ba1af6b59eaab
unknown <ngulalemulenga@gmail.com>
2024-11-12 18:06:23 -0500

    Added functionality to fetch data from the Database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,23 @@
 import express from 'express';
-import products from '../data/products.js';
+import Product from '../models/productModel.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', asyncHandler (async (req, res) => {
+    const products = await Product.find({});
     res.json(products)
-});
+}));
 
-router.get('/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-});
+router.get('/:id', asyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if(product) {
+        return res.json(product)
+    }
+    else {
+        res.status(404).json({ message: 'Product not found' })
+    }
+}));
 
 export default router;
